ca958c5f6cf740e3d679312742465a50b436f5da
ran.zhou <ran.zhou@aptiv.com>
2019-07-25 12:33:49 +0800

    Uploaded init_mercedes.sh

---
+++ RaspberryHASS/custom_components/mercedesmebeta/init_mercedes.sh
@@ -0,0 +1,5 @@
+!/usr/bin/bash
+wget https://raw.githubusercontent.com/rangerzhou/HomeAssistant/master/RaspberryHASS/custom_components/mercedesmebeta/__init__.py
+wget https://raw.githubusercontent.com/rangerzhou/HomeAssistant/master/RaspberryHASS/custom_components/mercedesmebeta/device_tracker.py
+wget https://raw.githubusercontent.com/rangerzhou/HomeAssistant/master/RaspberryHASS/custom_components/mercedesmebeta/sensor.py
+
