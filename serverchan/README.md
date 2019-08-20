4270abf95630110ec130e5461f54e1f07dc948aa
ran.zhou <ran.zhou@aptiv.com>
2019-08-20 13:21:24 +0800

    Uploaded serverchan for wechat push

---
+++ serverchan/README.md
@@ -0,0 +1,45 @@
+
+
+Server 酱介绍：http://sc.ftqq.com/3.version
+
+### 使用方法
+
+- 申请 sckey：http://sc.ftqq.com/?c=code
+
+- 绑定微信：http://sc.ftqq.com/?c=wechat&a=bind
+
+- 下载 `__init__.py` 和 `notify.py` ，放入 `homeassistant/custom_components/serverchan` 目录下
+
+- 配置 configurations.yaml
+
+  ``` yaml
+  notify:
+    - platform: serverchan
+      name: weixin
+      sc_key: !secret notify_serverchan_sc_key
+  ```
+
+### HA 使用
+
+调用服务 notify.weixin
+
+``` shell
+{"title":"Homeassistant","message":"来自HA的测试"}
+```
+
+
+
+此文使用组件参考：
+
+[解决Server酱微信推送功能](https://bbs.hassbian.com/forum.php?mod=viewthread&tid=3936&extra=&highlight=server%2B%E9%85%B1&page=1)
+
+
+
+其他参考：
+
+[Pushbear【微信推送消息】【推送图片】【www文件夹的用处】](https://bbs.hassbian.com/thread-2766-1-1.html)
+
+[使用PushBear一对多的微信推送](https://bbs.hassbian.com/thread-1650-1-1.html)
+
+[通过serverchan推送微信消息的简陋插件](https://bbs.hassbian.com/thread-1099-1-1.html)
+
---
+++ serverchan/notify.py
@@ -0,0 +1,54 @@
+"""
+Weixin Push Service by ServerChan
+"""
+import logging
+#from fake_useragent import UserAgent
+import datetime
+import requests
+import voluptuous as vol
+
+from homeassistant.components.notify import (
+    ATTR_MESSAGE, ATTR_TITLE, ATTR_DATA, ATTR_TARGET, PLATFORM_SCHEMA, BaseNotificationService)
+import homeassistant.helpers.config_validation as cv
+
+_LOGGER = logging.getLogger(__name__)
+_RESOURCE = 'https://sc.ftqq.com'
+
+CONF_SCKEY = 'sc_key'
+
+def get_service(hass, config, discovery_info=None):
+    """Get the ServerChan notification service."""
+    sc_key = config.get(CONF_SCKEY)
+    return ServerchanNotificationService(hass, sc_key)
+
+
+class ServerchanNotificationService(BaseNotificationService):
+    """Implementation of the notification service for SimplePush."""
+
+    def __init__(self, hass, sc_key):
+        """Initialize the service."""
+        _LOGGER.debug("INIT message")
+        self._sc_key = sc_key
+
+    def send_message(self, message='', **kwargs):
+        """Send a message to a user."""
+        url = '{}/{}.send'.format(_RESOURCE, self._sc_key)
+        title = kwargs.get(ATTR_TITLE)
+        if title:
+           timestp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+           sendmessage = '{} {}'.format(timestp, message)
+           payload = {'text': title, 'desp': sendmessage}
+           #ua = UserAgent()
+           #header = {'user-agent': ua.random }
+           response = requests.get(url,params = payload)
+           _LOGGER.debug("sneding out message")
+		
+           if response.status_code != 200:
+              obj = response.json()
+              error_message = obj['error']['message']
+              error_code = obj['error']['code']
+              _LOGGER.error(
+                   "Error %s : %s (Code %s)", resp.status_code, error_message,
+                   error_code)
+        else:
+           _LOGGER.error("Title can NOT be null")
